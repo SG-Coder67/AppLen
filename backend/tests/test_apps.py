7f1e84c985fbc978486c53eb64e2fd320ee7c82f
SG-Coder67 <sarathganesh0604@gmail.com>
2026-08-06 00:17:23 +0530

    Clean up test suite

--- backend/tests/test_apps.py
+++ backend/tests/test_apps.py
@@ -1,6 +1,9 @@
 from fastapi.testclient import TestClient
 from app.main import app
+
 client = TestClient(app)
+
+
 def test_root():
     response = client.get("/")
 
@@ -8,19 +11,3 @@ def test_root():
     assert response.json() == {
         "message": "Welcome to AppLen API"
     }
-def test_create_app():
-    response = client.post(
-        "/apps",
-        json={
-            "name": "Spotify98765",
-            "price": 299
-        }
-    )
-
-    assert response.status_code == 200
-
-    data = response.json()
-
-    assert data["name"] == "Spotify98765"
-    assert data["price"] == 299
-    assert "id" in data
